8716cc7f851f46f03d3af541fbaab2268308021d
Garima Chandra <143211271+Garima-149@users.noreply.github.com>
2026-04-09 15:45:28 +0530

    Update firebase.js

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,1 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAriWr6hak1TdMt9Pimu7KdJsxw2xQFPkE",
+  authDomain: "emotionflow-84a9d.firebaseapp.com",
+  projectId: "emotionflow-84a9d",
+  storageBucket: "emotionflow-84a9d.firebasestorage.app",
+  messagingSenderId: "640550930813",
+  appId: "1:640550930813:web:ab1b58b133df03fca36414",
+  measurementId: "G-2RWBHVHJ6H"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+
+
 
